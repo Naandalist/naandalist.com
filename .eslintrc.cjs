d0e9b5eeb8aa5787fa4fb9c36687146ee5165893
Naandalist <listiananda.apriliawan@gmail.com>
2025-11-12 22:42:55 +0700

    chore: refactor code structure with eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,15 +8,84 @@ module.exports = {
     "eslint:recommended",
     "plugin:astro/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
+  plugins: ["import"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./tsconfig.json",
+      },
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
   rules: {
     semi: ["error", "always"],
-    quotes: ["error", "double", { "allowTemplateLiterals": true }],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
     "@typescript-eslint/triple-slash-reference": "off",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+        ],
+        pathGroups: [
+          {
+            pattern: "astro",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "astro:*",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@components/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@layouts/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@lib/**",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@consts",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@types",
+            group: "internal",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
   },
   overrides: [
     {
