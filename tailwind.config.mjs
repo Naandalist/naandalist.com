6176fa2156a7df8502de28946df2cb7e44dce4b3
Naandalist <listiananda.apriliawan@gmail.com>
2025-11-16 22:13:19 +0700

    feat: update link colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,8 +11,8 @@ export default {
       },
       colors: {
         link: {
-          DEFAULT: "rgba(231, 76, 60,1.0)",
-          hover: "rgba(192, 57, 43,1.0)",
+          DEFAULT: "rgba(255, 56, 56,1.0)",
+          hover: "rgba(255, 77, 77,1.0)",
         },
       },
     },
